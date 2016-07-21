9630bdf5c0f03880c5acdaa17d1190c006ffb64c
Henk Mollema <henkmollema@gmail.com>
2016-07-21 20:16:38 +0200

    Add Web API sample project

---
+++ samples/WebApi/src/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public MyViewModel Get()
+        {
+            return new MyViewModel { Message = "Hello, world!" };
+        }
+    }
+
+    public class MyViewModel
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ samples/WebApi/src/WebApplication1/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddCors()
+                    .AddJsonFormatters();
+        }
+
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+{
+    app.UseCors(policy =>
+    {
+        policy.AllowAnyOrigin();
+    });
+    app.UseMvc();
+}
+    }
+}
